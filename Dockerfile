1b465818b0c8746902aa636ea174d14cd942d974
GopiPacha1996 <gopi.mdvk@gmail.com>
2020-09-18 15:49:48 +0530

    initial Commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:alpine
+
+WORKDIR /var/www
+
+COPY package.json .
+
+RUN npm install
+
+COPY . .
+
+CMD ["npm","start"]
+
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome Gopi Happy Learing</h2>
+        </div>
+        <p className="App-intro">
+          Just to test Volumes on Docker.And it is Working Perfect here .
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
